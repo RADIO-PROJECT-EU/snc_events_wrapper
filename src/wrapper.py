915781ef122e1c8f8d8b9f7b8783c6e06ed8273d
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-05-19 16:40:22 +0200

    Init and hopefully the only commit!

---
+++ src/wrapper.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import os
+import rospkg
+import roslib, rospy
+from datetime import datetime
+from snc_sensors_publisher.msg import SnCSensorsMsg
+
+queue = []
+
+def init():
+    dt = datetime.now()
+    start_time = dt.minute*60000000 + dt.second*1000000 + dt.microsecond
+    rospy.init_node('snc_events_wrapper')
+    topic = rospy.get_param("~events_topic", "/snc_sensors/events")
+    rospy.Subscriber(topic, SnCSensorsMsg, eventCallback)
+    while not rospy.is_shutdown():
+        rospy.spin()
+
+def eventCallback(msg):
+    dt = datetime.now()
+
+    #rospack = rospkg.RosPack()
+    #filename = 'official_log_chair_'+datetime.today().strftime("%d-%m-%Y")+'_'+dt.strftime("%H%M%S")+'.csv'
+    #logs_path = rospack.get_path('snc_events_wrapper') + '/logs/' + filename
+
+    for event in msg.sensors:
+
+        if event.name == 'No activity in room':
+            if not ('No activity in room' in queue):
+                rospack = rospkg.RosPack()
+                filename = 'official_log_presence_'+datetime.today().strftime("%d-%m-%Y")+'_'+dt.strftime("%H%M%S")+'.csv'
+                logs_path = rospack.get_path('snc_events_wrapper') + '/logs/' + filename
+                with open(logs_path,'ab+') as f:
+                    f.write("No presence in the room timestamp\n")
+                    f.write(event.status+'\n')
+        elif event.name == 'Goint out the room':
+            if not ('Goint out the room' in queue):
+                rospack = rospkg.RosPack()
+                filename = 'official_log_going_'+datetime.today().strftime("%d-%m-%Y")+'_'+dt.strftime("%H%M%S")+'.csv'
+                logs_path = rospack.get_path('snc_events_wrapper') + '/logs/' + filename
+                with open(logs_path,'ab+') as f:
+                    f.write("Going out of the room timestamp\n")
+                    f.write(event.status+'\n')
+        elif event.name == 'Coming in the room':
+            if not ('Coming in the room' in queue):
+                rospack = rospkg.RosPack()
+                filename = 'official_log_coming_'+datetime.today().strftime("%d-%m-%Y")+'_'+dt.strftime("%H%M%S")+'.csv'
+                logs_path = rospack.get_path('snc_events_wrapper') + '/logs/' + filename
+                with open(logs_path,'ab+') as f:
+                    f.write("Coming in the room timestamp\n")
+                    f.write(event.status+'\n')
+        elif 'Watching TV' in event.name:
+            if not ('Watching TV on chair' in queue or 'Watching TV on sofa' in queue):
+                rospack = rospkg.RosPack()
+                filename = 'official_log_tv_'+datetime.today().strftime("%d-%m-%Y")+'_'+dt.strftime("%H%M%S")+'.csv'
+                logs_path = rospack.get_path('snc_events_wrapper') + '/logs/' + filename
+                with open(logs_path,'ab+') as f:
+                    f.write("Watching TV timestamp\n")
+                    f.write(event.status+'\n')
+        elif event.name == 'Finish cooking':
+            if not ('Finish cooking' in queue):
+                rospack = rospkg.RosPack()
+                filename = 'official_log_cooking_'+datetime.today().strftime("%d-%m-%Y")+'_'+dt.strftime("%H%M%S")+'.csv'
+                logs_path = rospack.get_path('snc_events_wrapper') + '/logs/' + filename
+                with open(logs_path,'ab+') as f:
+                    f.write("Finished cooking timestamp\n")
+                    f.write(event.status+'\n')
+
+        queue.append(event.name)
+
+        if len(queue) > 5:
+            del queue[0]
+
+if __name__ == '__main__':
+    init()
